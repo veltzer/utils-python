1e332402a2f2bf9b0e7e73c600a432660554e5dc
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-06 14:13:09 +0300

    better deps

--- config/python.py
+++ config/python.py
@@ -2,10 +2,6 @@ import config.project
 
 package_name = config.project.project_name
 
-console_scripts = []
-
-setup_requires = []
-
 run_requires = [
     "pymakehelper",
     "jsonpickle",
@@ -38,13 +34,6 @@ dev_requires = [
     "black",
 ]
 
-install_requires = list(setup_requires)
-install_requires.extend(run_requires)
-
-extras_require = {
-}
-
 python_requires = ">=3.9"
 test_os = ["ubuntu-20.04"]
 test_python = ["3.9"]
-test_container = ["ubuntu:20.04"]
