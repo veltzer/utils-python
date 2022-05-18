c0363c5338fc2c111bea52c470df6864d0e77fb4
Mark Veltzer <mark.veltzer@gmail.com>
2022-05-18 13:51:15 +0300

    new config

--- config/project.py
+++ config/project.py
@@ -1,4 +1,1 @@
-project_name = "utils-python"
-project_long_description = "Various utilities written in python"
-project_year_started = 2019
-project_description = """Some utilities I wrote in python"""
+description_short = "Various utilities written in python"
