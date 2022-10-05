986ea1e4817dcdebd39f943b13f248f91d475a64
Mark Veltzer <mark.veltzer@gmail.com>
2022-10-05 18:43:25 +0300

    better

--- src/data_check_goodreads.py
+++ src/data_check_goodreads.py
@@ -12,14 +12,29 @@ import requests
 import bs4  # type: ignore
 
 
+SAVE_TEMP = True
+
+
 def get_goodreads_data(f_goodreads_id, session):
-    print(f"retrieving {f_goodreads_id}...")
+    # print(f"retrieving {f_goodreads_id}...")
     url = f"https://www.goodreads.com/book/show/{f_goodreads_id}"
     response = session.get(url)
     response.raise_for_status()
+    # print(response.content)
     soup = bs4.BeautifulSoup(response.content, "html.parser")
-    print(soup.text)
-    return {}
+    if SAVE_TEMP:
+        with open("/tmp/temp.html", "w") as file:
+            file.write(str(soup))
+    f_title = soup.find(id="bookTitle")
+    if f_title is None:
+        f_title = soup.find("h1", {"data-testid":"bookTitle"})
+        # print("title is ", f_title)
+    else:
+        # print(f_title)
+        f_title = f_title.text.strip()
+    return {
+        "title": f_title,
+    }
 
 
 def goodreads_id_to_goodreads_data(f_goodreads_id, cache, session):
@@ -43,12 +58,14 @@ def main():
             data = yaml.safe_load(stream)
         data = data["items"]
         for datum in data:
-            f_goodreads_id = datum["goodreads_id"]
-            f_name = datum["name"]
-            # print(f"doing [{f_name}] [{f_imdbid}]")
-            goodreads_data = goodreads_id_to_goodreads_data(f_goodreads_id, cache, session)
-            f_title = goodreads_data["title"]
-            assert f_title == f_name, f"{f_goodreads_id} {f_title} {f_name}"
+            for name in datum["names"]:
+                if "goodreads_id" in name:
+                    f_goodreads_id = name["goodreads_id"]
+                    f_name = name["name"]
+                    print(f"doing [{f_name}] [{f_goodreads_id}]")
+                    goodreads_data = goodreads_id_to_goodreads_data(f_goodreads_id, cache, session)
+                    f_title = goodreads_data["title"]
+                    assert f_title == f_name, f"{f_goodreads_id} {f_title} {f_name}"
     cache.close()
 
 
