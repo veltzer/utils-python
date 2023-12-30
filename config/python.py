44753a324b22273615fb3900f788aa1c89239ab4
Mark Veltzer <mark.veltzer@gmail.com>
2023-12-30 04:03:07 +0200

    no commit message given

--- config/python.py
+++ config/python.py
@@ -1,12 +1,11 @@
-make_requires = [
-    "pymakehelper",
-    "pydmt",
-    "pycmdtools",
-]
-dev_requires = [
+from typing import List
+
+
+config_requires: List[str] = []
+dev_requires: List[str] = [
     "black",
 ]
-install_requires = [
+install_requires: List[str] = [
     "jsonpickle",
     "PyGithub",
     "bsddb3",
@@ -15,7 +14,12 @@ install_requires = [
     # this does not work
     # "python-apt",
 ]
-test_requires = [
+make_requires: List[str] = [
+    "pymakehelper",
+    "pydmt",
+    "pycmdtools",
+]
+test_requires: List[str] = [
     "pylint",
     "pytest",
     "pytest-cov",
@@ -25,3 +29,4 @@ test_requires = [
     "types-chardet",
     "types-PyYAML",
 ]
+requires = config_requires + install_requires + make_requires + test_requires
