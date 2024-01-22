2e7c914839fee69515237eeacf473cb4f201498e
Mark Veltzer <mark.veltzer@gmail.com>
2024-01-22 07:08:46 +0200

    fixes to utility

--- src/utils_grep.py
+++ src/utils_grep.py
@@ -9,33 +9,39 @@ import sys
 import os
 import os.path
 
-# command line usage...
-if len(sys.argv) != 4:
-    print("usage: grep.py [expr] [fileregexp] [folder]")
-    sys.exit(1)
-# first compile the regular expression to search for...
-c = re.compile(sys.argv[1])
-cf = re.compile(sys.argv[2])
-folder = sys.argv[3]
-debug = False
-# debug=True
-printOnlyFiles = False
-printedFiles = set()
 
-for root, dirs, files in os.walk(folder):
-    for file in files:
-        full = os.path.join(root, file)
-        if debug:
-            print(f"file is [{full}]")
-        if cf.match(full):
+def main():
+    # command line usage...
+    if len(sys.argv) != 4:
+        print("usage: grep.py [expr] [fileregexp] [folder]")
+        sys.exit(1)
+    # first compile the regular expression to search for...
+    c = re.compile(sys.argv[1])
+    cf = re.compile(sys.argv[2])
+    folder = sys.argv[3]
+    debug = False
+    # debug=True
+    printOnlyFiles = False
+    printedFiles = set()
+
+    for root, _dirs, files in os.walk(folder):
+        for file in files:
+            full = os.path.join(root, file)
             if debug:
-                print(f"doing file [{full}]")
-            with open(full) as f:
-                for num, line in enumerate(f):
-                    for x in c.finditer(line):
-                        if printOnlyFiles:
-                            if full not in printedFiles:
-                                print(full)
-                                printedFiles.add(full)
-                        else:
-                            print(f"{full}, {num}: {line[:-1]}")
+                print(f"file is [{full}]")
+            if cf.match(full):
+                if debug:
+                    print(f"doing file [{full}]")
+                with open(full) as f:
+                    for num, line in enumerate(f):
+                        for _ in c.finditer(line):
+                            if printOnlyFiles:
+                                if full not in printedFiles:
+                                    print(full)
+                                    printedFiles.add(full)
+                            else:
+                                print(f"{full}, {num}: {line[:-1]}")
+
+
+if __name__ == "__main__":
+    main()
