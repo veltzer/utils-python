7d149a2e50e299f59d2c1dce2ee59e7fb782d613
Mark Veltzer <mark.veltzer@gmail.com>
2024-09-17 16:06:49 +0300

    no commit message given

--- src/ppt_to_text.py
+++ src/ppt_to_text.py
@@ -7,8 +7,9 @@ def odp_to_text(source, target):
         prs = Presentation(source)
         for slide in prs.slides:
             for shape in slide.shapes:
-                if hasattr(shape, 'text'):
-                    stream.write(shape.text+"\n")
+                if hasattr(shape, "text"):
+                    stream.write(shape.text + "\n")
+
 
 if __name__ == "__main__":
     odp_to_text(sys.argv[1], sys.argv[2])
