afd9933314e564c68a451c8db9525630e2dc573b
Mark Veltzer <mark.veltzer@gmail.com>
2025-05-30 17:41:00 +0300

    made utility better

--- src/utils_replace_first_line.py
+++ src/utils_replace_first_line.py
@@ -10,22 +10,22 @@ import os.path
 
 # command line usage...
 me = os.path.basename(sys.argv[0])
-if len(sys.argv) < 2:
-    print(f"usage: {me} [file] ...", file=sys.stderr)
+if len(sys.argv) < 3:
+    print(f"usage: {me} [firstline] [file] ...", file=sys.stderr)
+    print("firstline should come with no newline")
     sys.exit(1)
 
-replacement = "#!/usr/bin/python\n\n"
-files = sys.argv[1:]
-for file in files:
+replacement = sys.argv[1]
+for file in sys.argv[2:]:
     new_file = file + ".tmp"
     first = True
     with open(file, "r") as old_stream, open(new_file, "w") as new_stream:
         for line in old_stream:
             if first:
-                new_stream.write(replacement)
+                new_stream.write(replacement + "\n")
                 first = False
             else:
                 new_stream.write(line)
-    # switch the files
+    # switch old and new versions
     os.unlink(file)
     os.rename(new_file, file)
