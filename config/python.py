6efb2a88baf20887c411c8f7d375738b1dd5ebe0
Mark Veltzer <mark.veltzer@gmail.com>
2025-06-28 06:36:39 +0300

    modernize

--- config/python.py
+++ config/python.py
@@ -1,10 +1,4 @@
-from typing import List
-
-
-config_requires: List[str] = []
-dev_requires: List[str] = [
-]
-install_requires: List[str] = [
+install_requires: list[str] = [
     "jsonpickle",
     "PyGithub",
     "bsddb3",
@@ -18,13 +12,13 @@ install_requires: List[str] = [
     "python-dateutil",
     "ruamel.yaml",
 ]
-build_requires: List[str] = [
+build_requires: list[str] = [
     "pydmt",
     "pymakehelper",
     "pycmdtools",
     "pyclassifiers",
 ]
-test_requires: List[str] = [
+test_requires: list[str] = [
     "pylint",
     "pytest",
     "pytest-cov",
@@ -35,4 +29,4 @@ test_requires: List[str] = [
     "types-pytz",
     "types-python-dateutil",
 ]
-requires = config_requires + install_requires + build_requires + test_requires
+requires = install_requires + build_requires + test_requires
